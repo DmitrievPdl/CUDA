536d14970629cbec5a6b4090982c2cd44495c45e
dmitrpdl <63112883+DmitrievPdl@users.noreply.github.com>
2021-02-07 18:16:35 +0300

    Update main.cpp
    changed the construction of the histogram using the reduction algorithm and atomic addition

--- main.cpp
+++ main.cpp
@@ -18,94 +18,27 @@
 #include <curand.h>
 #include <curand_kernel.h>
 #include <random>
+#include <string> 
 #define ITERS_PER_RENDER 1
 
 float x(int col) {
-    return ( col * SCALE / (W / 2) - SCALE);
+    return ( col * SCALEX / (W / 2) - SCALEX);
 }
 float p(int row) {
-    return  (SCALE - row * SCALE / (W / 2));
+    return  (SCALEY - row * SCALEY / (W / 2));
 }
-void getParticalsForPlot(float3* particles) {
-    FILE* outfile = fopen("result.txt", "w");
-
+void getParticalsForPlot(float3* particles, int time) {
+    char str[20];
+    sprintf(str, "%d.txt", time);
+    FILE* outfile = fopen(str, "a");
     for (int i = 0; i < NUM; i++) {
         if(particles[i].x != -4)
-            fprintf(outfile, "%f \t %f \n", particles[i].x, particles[i].y);
-    }
-    fclose(outfile);
-}
-void findRight(float* density) {
-    float dp = SCALE / (H / 2);
-    float dx = SCALE / (W / 2);
-    FILE* outfile = fopen("result.txt", "w");
-    float f1 = 0.0f;
-    float Pleft = 0.0f;
-    float Pright = 0.0f;
-    for (int col = 500; col < 1500; col++) {
-        f1 = 0.0f;
-        Pleft = 0.0f;
-        Pright = 0.0f;
-        
-        for (int row = 500; row < 1500; row++) {
-            f1 += density[row * W + col] * dp;
-            Pleft += density[row * W + col - 1] * p(row) * p(row) * dp;
-            Pright += density[row * W + col + 1] * p(row) * p(row) * dp;
-        }
-        
-        fprintf(outfile, "%f \t %E \n", x(col), x(col));//1.0f / f1 * (Pleft - Pright) / (2.0f * dx));
+            fprintf(outfile, "%f \t %f \t %f \n ", particles[i].x, particles[i].y, particles[i].z);
     }
     fclose(outfile);
 }
 
-void findAveragetP(float* density) {
-    float dp = SCALE / (H / 2);
-    float dx = SCALE / (W / 2);
-    FILE* outfile = fopen("result.txt", "w");
-    float averagetV = 0.0f;
-    float f1 = 0.0f;
 
-    for (int col = 500; col < 1500; col++) {
-        averagetV = 0.0f;
-        f1 = 0.0f;
-
-        for (int row = 0; row < 2000; row++) {
-            averagetV += density[row * W + col] * p(row) * dp ;
-        }
-        for (int row = 0; row < 2000; row++) {
-            f1 += density[row * W + col] * dp;
-        }
-        averagetV *= 1.0f / f1;
-        fprintf(outfile, "%f \t %E \n", x(col), averagetV);
-    }
-    fclose(outfile);
-}
-void findTotalN(float* density) {
-    float dp = SCALE / (H / 2);
-    float dx = SCALE / (W / 2);
-    FILE* outfile = fopen("result.txt", "at");
-    float f1 = 0.0f;
-    float TotalN = 0.0f;
-    float dns = 0.0f;
-    for (int col = 0; col < 2000; col++){
-        for (int row = 0; row < 2000; row++) {
-            dns += density[row * W + col] * NUM;
-        }
-    }
-    printf("%f \n",dns);
-    fprintf(outfile, "%f \n", dns);
-    fclose(outfile);
-}
-void findTotalNgpu(float3* particles) {
-    FILE* outfile = fopen("result.txt", "at");
-    int totalN = 0;
-    for (int i = 0; i < NUM; i++) {
-        if (particles[i].z != -1)
-            totalN += 1;
-    }
-    fprintf(outfile, "%d \n", totalN);
-    fclose(outfile);
-}
 // texture and pixel objects
 GLuint pbo = 0; // OpenGL pixel buffer object
 GLuint tex = 0; // OpenGL texture object
@@ -117,37 +50,18 @@ void render() {
     cudaGraphicsMapResources(1, &cuda_pbo_resource, 0);
     cudaGraphicsResourceGetMappedPointer((void**)&d_out, NULL, cuda_pbo_resource);
 
-    for (int i = 0; i < ITERS_PER_RENDER; ++i) 
-        kernelLauncher(d_out, d_particals, d_density, W, H, sys, d_num);
-    
-    if ( iterationCount == 8000) {
-        float dx = SCALE / (W / 2);
-        float dp = SCALE / (W / 2);
+    for (int i = 0; i < ITERS_PER_RENDER; ++i) {
+        if (iterationCount  < 7100)
+            kernelLauncher(d_out, d_particals, d_density, d_doubderv, d_dnscell, W, H);
+    }
+   
+    if (iterationCount == 4000 || iterationCount == 5000 || iterationCount == 6000) {
         // get particals for plot
 
         particles = new float3[NUM];
         cudaMemcpy(particles, d_particals, NUM * sizeof(float3), cudaMemcpyDeviceToHost);
-        getParticalsForPlot(particles);
+        getParticalsForPlot(particles, iterationCount);
         free(particles);
-
-        // get density for calculation
-        /*
-        //density = new float[W * H];
-        cudaMemcpy(density, d_density, W * H * sizeof(float), cudaMemcpyDeviceToHost);
-
-        //findRight(density);
-        findAveragetV(density);
-        free(density);
-        */
-        // find avarage V
-        //density = new float[W * H];
-        //cudaMemcpy(density, d_density, W * H * sizeof(float), cudaMemcpyDeviceToHost);
-        //findTotalN(density);
-
-        //particles = new float3[NUM];
-        //cudaMemcpy(particles, d_particals, NUM * sizeof(float3), cudaMemcpyDeviceToHost);
-        //findTotalNgpu(particles);
-        //free(particles);
     }
     cudaGraphicsUnmapResources(1, &cuda_pbo_resource, 0);
     char title[128];
@@ -204,13 +118,17 @@ void exitfunc() {
     
     cudaFree(d_density); 
     cudaFree(d_particals);
+    cudaFree(d_doubderv);
+    cudaFree(d_dnscell);
 }
 
 int main(int argc, char** argv) {
     cudaMalloc(&d_density, W * H * sizeof(float));
     cudaMalloc(&d_particals, NUM * sizeof(float3));
-    cudaMalloc(&d_num, sizeof(int));
-    initialConditions(d_out, d_particals, d_density, d_num, W, H);
+    cudaMalloc((void**)&d_doubderv, NUM * sizeof(float));
+    cudaMalloc((void**)&d_dnscell, NUM * sizeof(float));
+
+    initialConditions(d_out, d_particals, d_density, W, H);
 
     initGLUT(&argc, argv);
     gluOrtho2D(0, W, H, 0);

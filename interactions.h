af12b046d73cd8938866e9d6e12b9cf49812086d
dmitrpdl <63112883+DmitrievPdl@users.noreply.github.com>
2021-02-07 18:17:04 +0300

    Update interactions.h
    changed the construction of the histogram using the reduction algorithm and atomic addition

--- interactions.h
+++ interactions.h
@@ -10,21 +10,21 @@
 #include <GL/freeglut.h>
 #endif
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
-#define W 2000
-#define H 2000
-#define NUM  1000 * 1000 * 4
+#define W 1000
+#define H 1000
+#define NUM  256 * 16
+#define SQRTNUM 16 * 4
 #define DT 1.f // source intensity increment
-// if sys == 0  plot density 
-// if sys == 1  plot trace
 int sys = 0;
-
+float* d_dnscell;
+float* d_doubderv;
 float* d_density = 0;
 float* density = 0;
 float3* particles = 0;
 float3* d_particals = 0;
-float* d_time = 0;
+
 uchar4* d_out = 0;
-int* d_num = 0;
+
 
 int iterationCount = 0;
 

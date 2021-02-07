9386e5490f263080a2d6881fefe2a75648e83244
dmitrpdl <63112883+DmitrievPdl@users.noreply.github.com>
2021-02-07 18:15:59 +0300

    Update kernel.h
    changed the construction of the histogram using the reduction algorithm and atomic addition

--- kernel.h
+++ kernel.h
@@ -7,14 +7,18 @@
 struct uchar4;
 struct float3; // particles x and v
 struct float2; // system time and number of particles
-#define NUM  1000 * 1000 * 4
-#define SCALE 2.0f
-#define STEPTIME 0.001f
-#define MASS 1.0f
-#define PLANK 0.000f
-#define APOT -0.8f
-#define BPOT 0.8f
-void kernelLauncher(uchar4* d_out, float3* d_particals, float* d_density, int w, int h, int sys, int* d_num);
-void initialConditions(uchar4* d_out, float3* d_particals, float* d_density, int* d_num, int w, int h);
+#define NUM  256 * 16
+#define SQRTNUM 16 * 4
+#define SCALEY 20.0f
+#define SCALEX 1.5f
+#define STEPTIME 0.1f
+#define MASS 2000
+#define PLANK 1.0f
+#define BPOT 0.2981f
+#define APOT 0.2f
+#define HP 0.5f
+#define HX 0.05f
+void kernelLauncher(uchar4* d_out, float3* d_particals, float* d_density, float* d_doubderv, float * d_dnscell, int w, int h);
+void initialConditions(uchar4* d_out, float3* d_particals, float* d_density, int w, int h);
 #endif
 
